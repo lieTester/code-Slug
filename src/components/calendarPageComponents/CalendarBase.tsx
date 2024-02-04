0bd4334517ece6de250afd03d3f59f2c270c7012
puneet khandal <lietester.00@gmail.com>
2024-02-04 07:30:00 +0530

    calendarbase added base layout

--- src/components/calendarPageComponents/CalendarBase.tsx
+++ src/components/calendarPageComponents/CalendarBase.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { IoMdArrowDropleft, IoMdArrowDropright } from "react-icons/io";
 
 type CalendarCell = {
@@ -7,41 +7,45 @@ type CalendarCell = {
 };
 
 const CalenderBase: React.FC = () => {
-   const [currentMonth, setCurrentMonth] = useState(new Date());
-   const [presentMonth, setPresentMonth] = useState(new Date()); // for when we change month today date higlet only in it
+   const [currentMonth, setCurrentMonth] = useState<Date>(new Date());
+   const [presentMonth] = useState<Date>(new Date());
+   const gridRef = useRef<HTMLDivElement | null>(null);
+   const [hoveredElements, setHoveredElements] = useState<HTMLElement[]>([]);
+
+   const offset = 79;
+   const borderWidth = 3;
+   const angles: number[] = [];
+   for (let i = 0; i <= 2; i += 0.25) {
+      angles.push(Math.PI * i);
+   }
 
    const getDaysInMonth = (date: Date): CalendarCell[] => {
       const month = date.getMonth();
       const year = date.getFullYear();
-
-      // Find the first day of the month
       const firstDay = new Date(year, month, 1);
-      // Adjusted start day to Monday
-      const startDayOfWeek = (firstDay.getDay() + 7) % 7; // Adjust so Monday is 0
+      const startDayOfWeek = (firstDay.getDay() + 7) % 7;
 
       let days: CalendarCell[] = [];
-
-      // Calculate days needed to fill the first row from Monday
       const daysBeforeFirst = startDayOfWeek === 0 ? 6 : startDayOfWeek - 1;
 
-      // Fill the days before the first day of the month
       for (let i = daysBeforeFirst; i > 0; i--) {
-         const day = new Date(year, month, -i + 1);
-         days.push({ date: day, isInCurrentMonth: false });
+         days.push({
+            date: new Date(year, month, -i + 1),
+            isInCurrentMonth: false,
+         });
       }
 
-      // Get all days in the month
       const lastDay = new Date(year, month + 1, 0);
       for (let i = 1; i <= lastDay.getDate(); i++) {
          days.push({ date: new Date(year, month, i), isInCurrentMonth: true });
       }
 
-      // Fill the trailing days to complete the calendar grid
-      const lastDayOfWeek = lastDay.getDay();
       const daysAfterLast = 42 - days.length;
       for (let i = 1; i <= daysAfterLast; i++) {
-         const day = new Date(year, month + 1, i);
-         days.push({ date: day, isInCurrentMonth: false });
+         days.push({
+            date: new Date(year, month + 1, i),
+            isInCurrentMonth: false,
+         });
       }
 
       return days;
@@ -62,8 +66,62 @@ const CalenderBase: React.FC = () => {
       setCurrentMonth(newMonth);
    };
 
+   const clearNearbyStyles = () => {
+      hoveredElements.forEach((element) => {
+         element.style.borderImage = ""; // Set to empty string to clear the style
+      });
+      setHoveredElements([]);
+   };
+
+   useEffect(() => {
+      const handleMouseMove = (e: MouseEvent) => {
+         const x = e.clientX;
+         const y = e.clientY;
+
+         clearNearbyStyles(); // Clear previous effects
+
+         let nearbyElements: HTMLElement[] = [];
+
+         angles.forEach((rad) => {
+            const offsets = [offset * 0.35, offset * 1.105];
+            offsets.forEach((o) => {
+               const cx = Math.floor(x + Math.cos(rad) * o);
+               const cy = Math.floor(y + Math.sin(rad) * o);
+               const element = document.elementFromPoint(cx, cy) as HTMLElement;
+
+               if (
+                  element &&
+                  element.classList.contains("day-btn") &&
+                  !element.classList.contains("win-btn-active") &&
+                  !element.classList.contains("win-btn-selected") &&
+                  !nearbyElements.includes(element)
+               ) {
+                  const brect = element.getBoundingClientRect();
+                  const bx = x - brect.left; // x position within the element.
+                  const by = y - brect.top; // y position within the element.
+                  const gr = Math.floor(offset * 1.7);
+                  element.style.borderImage = `radial-gradient(${gr}px ${gr}px at ${bx}px ${by}px ,#a0aec0,#a0aec0,transparent ) 9 / ${borderWidth}px / 0px stretch`;
+                  nearbyElements.push(element);
+               }
+            });
+         });
+
+         setHoveredElements(nearbyElements);
+      };
+
+      const grid = gridRef.current;
+      grid?.addEventListener("mousemove", handleMouseMove);
+
+      return () => {
+         grid?.removeEventListener("mousemove", handleMouseMove);
+      };
+   }, [angles, hoveredElements]);
+
    return (
-      <div className="w-full h-full  text-white p-4 rounded-lg">
+      <div
+         ref={gridRef}
+         className="w-full h-full md:w-[65%] lg:w-[70%] 2xl:w-[75%] font-baloo text-white p-4 rounded-lg"
+      >
          <div className="h-[7%] flex justify-between items-center mb-4">
             <button onClick={() => changeMonth(-1)} className="text-xl">
                <IoMdArrowDropleft />
@@ -76,7 +134,7 @@ const CalenderBase: React.FC = () => {
                <IoMdArrowDropright />
             </button>
          </div>
-         <div className="grid grid-cols-7 gap-1 h-[93%]">
+         <div className="grid grid-cols-7 gap-[2px] h-[93%]">
             {["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"].map((day) => (
                <div key={day} className="text-center h-full">
                   {day}
@@ -85,15 +143,15 @@ const CalenderBase: React.FC = () => {
             {days.map((day, index) => (
                <div
                   key={index}
-                  className={`relative z-10 hover:border- after:rounded-sm hover:border-white border-transparent border-[1px] after:-z-10 after:absolute after:top-0 after:left-0 after:w-full after:h-full ${
+                  className={`relative day-btn z-10  after:rounded-sm hover:border-white border-transparent border-[3px] after:-z-10 after:absolute after:top-0 after:left-0 after:w-full after:h-full ${
                      day.date.getDate() === presentMonth.getDate() &&
                      currentMonth.getMonth() === presentMonth.getMonth() &&
                      currentMonth.getFullYear() === presentMonth.getFullYear()
-                        ? "bg-secod1 border-white"
+                        ? "bg-secod1 border-[#a0aec0]"
                         : `after:bg-blue-950`
-                  } after:backdrop-filter after:backdrop-blur-sm py-2 text-center h-full flex items-center justify-center  ${
+                  } after:backdrop-filter after:backdrop-blur-sm py-2 text-center h-full flex items-center justify-center ${
                      day.isInCurrentMonth
-                        ? "after:bg-opacity-40"
+                        ? "after:bg-opacity-50"
                         : "after:bg-opacity-20"
                   }`}
                >
--- src/components/calendarPageComponents/CalendarSidebar.tsx
+++ src/components/calendarPageComponents/CalendarSidebar.tsx
@@ -1,24 +1,103 @@
 import React from "react";
+import Image from "next/image";
 
 function calederSidebar() {
    return (
-      <section className="w-[100%] h-full md:w[35%] lg:w-[30%] 2xl:[25%] mr-2 text-prim1  mb-4 ">
-         <div className="w-full h-full grid grid-rows-4 gap-2 [&>*]:bg-backg2">
-            <div className="w-full h-full rounded-md py-2 px-2" id="highlight1">
-               Study Plans
-            </div>
-
-            <div className="w-full h-full rounded-md py-2 px-2" id="highlight2">
-               Study Plans
-            </div>
-
-            <div className="w-full h-full rounded-md py-2 px-2" id="highlight3">
-               Study Plans
-            </div>
-
-            <div className="w-full h-full rounded-md py-2 px-2" id="highlight4">
-               Study Plans
-            </div>
+      <section className="w-full h-full md:w-[40%] lg:w-[30%] xl:w-[25%]  mr-2 text-prim1  mb-4 ">
+         <div className="w-full [&>*]:bg-backg2 font-sofiaPro [&>*]:mb-2 [&>*]:pb-2 [&>*]:cursor-pointer">
+            <ul
+               className="relative w-[100%] h-full rounded-md overflow-hidden "
+               id="highlight1"
+            >
+               <li className="absolute w-full h-full z-1">
+                  <Image
+                     className="w-full h-full"
+                     src={"/img/bg1.jpg"}
+                     alt="logo-img"
+                     width={"1674"}
+                     height={"980"}
+                  />
+               </li>
+               <ul className="relative w-full h-full z-2 p-2 pt-5 ">
+                  <li className="text-xl">Calender Submits :</li>
+                  <li className="text-sm font-thin text-prim2 ">
+                     All solved and attemptes
+                  </li>
+                  <li className="relative text-sm font-thin  mt-3 bg-white text-secod2 inline-block p-1 rounded-md  !mr-auto">
+                     Check out
+                  </li>
+               </ul>
+            </ul>
+            <ul
+               className="relative w-full h-full rounded-md overflow-hidden"
+               id="highlight1"
+            >
+               <li className="absolute w-full h-full z-1">
+                  <Image
+                     className="w-full h-full"
+                     src={"/img/bg6.jpg"}
+                     alt="logo-img"
+                     width={"1674"}
+                     height={"980"}
+                  />
+               </li>
+               <ul className="relative w-full h-full z-2 p-2 pt-5 ">
+                  <li className="text-xl">Create & Edit week plans : </li>
+                  <li className="text-sm font-thin text-prim2 ">
+                     Set your weekly plans to consistently solve every Data &
+                     Structure
+                  </li>
+                  <li className="relative text-sm font-thin  mt-3 bg-white text-secod2 inline-block p-1 rounded-md  !mr-auto">
+                     Check out
+                  </li>
+               </ul>
+            </ul>
+            <ul
+               className="relative w-full h-full rounded-md overflow-hidden"
+               id="highlight1"
+            >
+               <li className="absolute w-full h-full z-1">
+                  <Image
+                     className="w-full h-full"
+                     src={"/img/bg3.jpg"}
+                     alt="logo-img"
+                     width={"1674"}
+                     height={"980"}
+                  />
+               </li>
+               <ul className="relative w-full h-full z-2 p-2 pt-5 ">
+                  <li className="text-xl">Select a weekly plan :</li>
+                  <li className="text-sm font-thin text-prim2 mt-1">
+                     Aplly plan to timely practice Data & Structure
+                  </li>
+                  <li className="relative text-sm font-thin  mt-3 bg-white text-secod2 inline-block p-1 rounded-md  !mr-auto">
+                     Check out
+                  </li>
+               </ul>
+            </ul>
+            <ul
+               className="relative w-full h-full rounded-md overflow-hidden"
+               id="highlight1"
+            >
+               <li className="absolute w-full h-full z-1">
+                  <Image
+                     className="w-full h-full"
+                     src={"/img/bg7.jpg"}
+                     alt="logo-img"
+                     width={"1674"}
+                     height={"980"}
+                  />
+               </li>
+               <ul className="relative w-full h-full z-2 p-2 pt-5 ">
+                  <li className="text-xl">Calender Submits :</li>
+                  <li className="text-sm font-thin text-prim2 mt-1">
+                     All solved and attemptes
+                  </li>
+                  <li className="relative text-sm font-thin  mt-3 bg-white text-secod2 inline-block p-1 rounded-md  !mr-auto">
+                     Check out
+                  </li>
+               </ul>
+            </ul>
          </div>
       </section>
    );
